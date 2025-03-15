f4ea60852f505cd73f25593041fbfb160ac84838
nodejsdeveloper1 <130368824+nodejsdeveloper1@users.noreply.github.com>
2025-03-15 12:51:32 +0530

    Update appconfig.ts

--- src/config/appconfig.ts
+++ src/config/appconfig.ts
@@ -13,9 +13,5 @@ export  let config = {
         },
         text: process.env.MAIL_TEXT || 'Hello, this is a test email!',
         subject: process.env.MAIL_SUBJECT || 'Test Email',
-   },
-    jwt: {
-        secret: process.env.JWT_SECRET ? process.env.JWT_SECRET : 'your_jwt_secret_key',
-        expiresIn: process.env.JWT_EXPIRES_IN || '1h',
-    }
+   }
 }
